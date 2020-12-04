c691f3b7d279737e46f4b895310a294e88e75865
itsinz <53069682+itsinz@users.noreply.github.com>
2020-12-04 23:37:25 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    if (step < 10) {
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Sad)
+        }
+    } else {
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    step = 0
+})
 input.onGesture(Gesture.Shake, function () {
     step += 1
     led.stopAnimation()
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (step < 10) {
-        basic.showIcon(IconNames.Sad)
-        led.stopAnimation()
-    } else {
-        basic.showIcon(IconNames.Happy)
-        led.stopAnimation()
-    }
-})
 let step = 0
 step = 0
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     basic.showNumber(step)
 })
